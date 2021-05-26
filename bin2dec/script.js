f6a64b9458eef193329e324165fb11b082f9ba87
Mitsrael Souza <mitsrael9@gmail.com>
2021-05-26 15:23:37 -0300

    bin to dec is now working

--- bin2dec/script.js
+++ bin2dec/script.js
@@ -4,23 +4,20 @@ let textarea = document.getElementsByTagName("textarea")
     for (let i = 0; i < textarea.length; i++) {
         changePlaceholder(textarea[i])
         textarea[i].setAttribute("onkeyup", "changePlaceholder(this)")
+
+        getNumbers(textarea[i])
+        textarea[i].setAttribute("onkeyup", "getNumbers(this)")
     }
 }
 
-function onlynumber(event) {
-    var keyPressed = event || window.event;
-    var key = keyPressed.keyCode || keyPressed.which;
+function getNumbers(numbers) {
+    var binary = numbers.value
+    var decimal = document.getElementById("decimalInput")
 
-    key = String.fromCharCode( key );
-
-    var regex = /^[0-1.]+$/;
-
-    if( !regex.test(key) ) {
-        keyPressed.returnValue = false;
-        if(keyPressed.preventDefault) keyPressed.preventDefault();
+    if(binary != "") {
+        decimal.value = parseInt(binary,2)
     }
 }
-
 
 function changePlaceholder(state) {
     if (state.value === ''){
@@ -31,3 +28,20 @@ function changePlaceholder(state) {
         state.classList.add("normal")
     }
 }
+
+function checkTextareaValue(event) {
+    var keyPressed = event || window.event
+    var key = keyPressed.keyCode || keyPressed.which
+
+    key = String.fromCharCode( key )
+
+    var regex = /^[0-1.]+$/
+
+    if( !regex.test(key) ) {
+        if(keyPressed.preventDefault) {
+            keyPressed.preventDefault()
+        }
+    }
+}
+
+
